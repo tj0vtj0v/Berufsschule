175b6c66145e35006cf45998e770993e0951c7a4
tj0vtj0v <burdorftjorven@gmail.com>
2023-05-16 07:58:06 +0200

    homework done in 08a_MySQL_Aufg_ForeignKey_7a-7e.sql

--- AEP/SQL/08a_MySQL_Aufg_ForeignKey_7a-7e.sql
+++ AEP/SQL/08a_MySQL_Aufg_ForeignKey_7a-7e.sql
@@ -1,6 +1,3 @@
-﻿# MySQL-Aufgaben
-# ==============
-
 DROP DATABASE IF EXISTS SchuelerDB;
 
 CREATE DATABASE SchuelerDB;
@@ -40,6 +37,10 @@ CREATE TABLE IF NOT EXISTS Schueler
 	AUTO_INCREMENT = 1000
 	COMMENT = "Diese Tabelle ist nicht voll normalisiert!";
 
+
+
+
+
 # 7a. Aufgabe
 # ----------
 # Erstellen Sie eine neue Tabelle Account mit den Spalten:
@@ -53,12 +54,35 @@ CREATE TABLE IF NOT EXISTS Schueler
 
 SELECT "Tabelle Account anlegen" as "Aufgabe 7a";
 
+CREATE TABLE IF NOT EXISTS Ownerlist(
+	Name			VARCHAR(30)	PRIMARY KEY,
+    INDEX name(Name)
+	);
+
+CREATE TABLE Account
+	(AccountId		INTEGER		PRIMARY KEY,#		AUTO_INCREMENT,
+	Owner			VARCHAR(25),
+    Benutzername	VARCHAR(25) UNIQUE KEY,
+    PWD				VARCHAR(50),
+    INDEX AId(AccountId),
+    FOREIGN KEY(Owner) REFERENCES Ownerlist(Name)
+    );
+
+
 
 
 # 7b) Überprüfen Sie das Anlegen des Fremdschlüssels mit Hilfe von  SHOW CREATE
 #	   Wie wird der Fremdschlüssel bezeichnet?
 
 SELECT "Show create von Account" as "Aufgabe 7b";
+
+SHOW CREATE TABLE Account;
+#CONSTRAINT `account_ibfk_1` FOREIGN KEY (`Owner`) REFERENCES `ownerlist` (`Name`)
+#'Schueler', 'Account', 'CREATE TABLE `account` (\n  `AccountId` int NOT NULL,\n  `Owner` varchar(25) DEFAULT NULL,\n  `Benutzername` varchar(25) DEFAULT NULL,\n  `PWD` varchar(50) DEFAULT NULL,\n  PRIMARY KEY (`AccountId`),\n  UNIQUE KEY `Benutzername` (`Benutzername`),\n  KEY `AId` (`AccountId`),\n  KEY `Owner` (`Owner`),\n  CONSTRAINT `account_ibfk_1` FOREIGN KEY (`Owner`) REFERENCES `ownerlist` (`Name`)\n) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci'
+
+
+
+
 
 
 # 7c) Sie können sich die vorhandenen Fremdschlüssel auch mit folgendem 
@@ -69,6 +93,12 @@ SELECT "Show create von Account" as "Aufgabe 7b";
 
 SELECT "Fremdschluessel mit dem Information_schema anzeigen" as "Aufgabe 7c";
 
+SELECT *
+FROM information_schema.table_constraints
+WHERE table_name='Account';
+
+
+
 
 # 7d) Löschen Sie die Tabelle Schueler.
 # 	Was stellen Sie fest?
@@ -76,12 +106,17 @@ SELECT "Fremdschluessel mit dem Information_schema anzeigen" as "Aufgabe 7c";
 
 SELECT "Loeschversuch von Schueler " as "Aufgabe 7d";
 	
-	
+
+DROP TABLE IF EXISTS Schueler;
+
+
+
 # 7e) Löschen Sie die Tabelle Account wieder.
 
 SELECT "Loeschen von" as "Aufgabe 7e";
 
+DROP TABLE IF EXISTS Account;
+DROP TABLE IF EXISTS Ownerlist;
 
-
-
+SHOW TABLES;
 
